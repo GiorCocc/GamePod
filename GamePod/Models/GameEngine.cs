4cb5e18de303f887c0ac7bef5e0db3d3ec37e8ce
giorcocc <giorcocc@gmail.com>
2023-09-25 12:34:42 +0200

    model

---
+++ GamePod/Models/GameEngine.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePod.Models;
+
+/*
+ *  GameEngine.cs
+ *  @Author: Giorgio Coccapani
+ *  @Date: 25/09/2023
+ *  
+ *  This class contains all the game engines that are supported by the GamePods.
+ *  For every game engine there is a name and the code of the download command
+ */
+
+internal class GameEngine
+{
+    // Game engines
+    public static readonly GameEngine Unity        = new("Unity", "unity-hub");
+    public static readonly GameEngine Godot        = new("Godot", "godot");
+    public static readonly GameEngine UnrealEngine = new("Unreal Engine", "unreal-engine");
+
+    // Properties
+    public string Name
+    {
+        get; private set;
+    }
+    public string DownloadCommand
+    {
+        get; private set;
+    }
+
+    // Constructor
+    private GameEngine(string name, string downloadCommand)
+    {
+        Name = name;
+        DownloadCommand = downloadCommand;
+    }
+
+    // Get the GameEngine object from the name
+    public static GameEngine GetGameEngine(string name)
+    {
+        switch (name)
+        {
+            case "Unity":
+                return Unity;
+            case "Godot":
+                return Godot;
+            case "Unreal Engine":
+                return UnrealEngine;
+            default:
+                throw new ArgumentException("The game engine " + name + " is not supported");
+        }
+    }
+
+    // Get the list of the names of the game engines
+    public static List<string> GetGameEngineNamesList()
+    {
+        List<string> names = new List<string>();
+
+        names.Add(Unity.Name);
+        names.Add(Godot.Name);
+        names.Add(UnrealEngine.Name);
+
+        return names;
+
+    }
+}
---
+++ GamePod/Models/LinuxDistribution.cs
@@ -0,0 +1,97 @@
+﻿namespace GamePod.Models;
+
+/*
+ *  LinuxDistributions.cs
+ *  @Author: Giorgio Coccapani
+ *  @Date: 25/09/2023
+ *  
+ *  This class contains all the Linux distributions that are supported by the GamePods.
+ *  For every linux distro there is a name, the code for the container and che command for the package manager.
+ */
+
+internal class LinuxDistribution
+{
+    // Linux distributions
+    public static readonly LinuxDistribution Ubuntu   = new("Ubuntu", "ubuntu", "apt-get");
+    public static readonly LinuxDistribution Debian   = new("Debian", "debian", "apt-get");
+    public static readonly LinuxDistribution Fedora   = new("Fedora", "fedora", "dnf");
+    public static readonly LinuxDistribution Arch     = new("Arch", "archlinux", "pacman");
+    public static readonly LinuxDistribution Manjaro  = new("Manjaro", "manjaro", "pacman");
+    public static readonly LinuxDistribution OpenSuse = new("OpenSuse", "opensuse", "zypper");
+    public static readonly LinuxDistribution CentOS   = new("CentOS", "centos", "yum");
+    public static readonly LinuxDistribution Gentoo   = new("Gentoo", "gentoo", "emerge");
+    public static readonly LinuxDistribution Void     = new("Void", "void", "xbps-install");
+    public static readonly LinuxDistribution Alpine   = new("Alpine", "alpine", "apk");
+
+    // Properties
+    public string Name
+    {
+        get; private set;
+    }
+    public string ContainerCode
+    {
+        get; private set;
+    }
+    public string PackageManager
+    {
+        get; private set;
+    }
+
+    // Constructor
+    private LinuxDistribution(string name, string containerCode, string packageManager)
+    {
+        Name = name;
+        ContainerCode = containerCode;
+        PackageManager = packageManager;
+    }
+
+    // Get the LinuxDistro object from the name
+    public static LinuxDistribution GetLinuxDistro(string name)
+    {
+        switch (name)
+        {
+            case "Ubuntu":
+                return Ubuntu;
+            case "Debian":
+                return Debian;
+            case "Fedora":
+                return Fedora;
+            case "Arch":
+                return Arch;
+            case "Manjaro":
+                return Manjaro;
+            case "OpenSuse":
+                return OpenSuse;
+            case "CentOS":
+                return CentOS;
+            case "Gentoo":
+                return Gentoo;
+            case "Void":
+                return Void;
+            case "Alpine":
+                return Alpine;
+            default:
+                return null;
+        }
+    }
+
+    // Get a list of all the Names of the LinuxDistro objects
+    public static List<string> GetLinuxDistroNamesList()
+    {
+        List<string> names = new List<string>();
+
+        names.Add(Ubuntu.Name);
+        names.Add(Debian.Name);
+        names.Add(Fedora.Name);
+        names.Add(Arch.Name);
+        names.Add(Manjaro.Name);
+        names.Add(OpenSuse.Name);
+        names.Add(CentOS.Name);
+        names.Add(Gentoo.Name);
+        names.Add(Void.Name);
+        names.Add(Alpine.Name);
+
+        return names;
+    }
+
+}
