c2bdd8a6d464b336c8fef7f4d25df20698d91784
giorcocc <giorcocc@gmail.com>
2023-10-12 15:56:01 +0200

    new page to modify the final run command

---
+++ GamePod/Views/AdvancedContainerCreationPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+using GamePod.ViewModels;
+
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+
+namespace GamePod.Views;
+
+public sealed partial class AdvancedContainerCreationPage : Page
+{
+    public AdvancedContainerCreationViewModel ViewModel
+    {
+        get;
+    }
+
+    public AdvancedContainerCreationPage()
+    {
+        ViewModel = App.GetService<AdvancedContainerCreationViewModel>();
+        InitializeComponent();
+    }
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+        if (e.Parameter is string && !string.IsNullOrWhiteSpace((string)e.Parameter))
+        {
+            // add the text to the editor rich text box in order to be modified
+            var command = FormatCommand((string)e.Parameter);
+            editor.Document.SetText(Microsoft.UI.Text.TextSetOptions.None, command);
+        }
+
+        base.OnNavigatedTo(e);
+    }
+
+    // TODO: separare ogni parametro del comando (ovvero gli elementi che iniziano con " --" o con " -") con un a capo e un segno di tabulazione
+    private string FormatCommand(string parameter)
+    {
+        var command = parameter;
+
+        return command;
+    }
+}
