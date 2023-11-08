3a99bdfae755ccead32735a1cf400d94203c3503
giorcocc <giorcocc@gmail.com>
2023-11-08 18:54:29 +0100

    start page layout

--- GamePod/ViewModels/ContainerDetailsViewModel.cs
+++ GamePod/ViewModels/ContainerDetailsViewModel.cs
@@ -1,14 +1,24 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using GamePod.Services;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
+﻿using System.Diagnostics;
+using CommunityToolkit.Mvvm.ComponentModel;
+using GamePod.Contracts.Services;
+using GamePod.Models;
 
 namespace GamePod.ViewModels;
 
 public partial class ContainerDetailsViewModel : ObservableRecipient
 {
+    private IDockerService service;
+
     public ContainerDetailsViewModel()
     {
-        
+        service = App.GetService<IDockerService>();
+    }
+
+    internal async Task<Container> GetContainer(string? containerName)
+    {
+        Container container = new Container(containerName);
+        await container.GetInspectResponse(container);
+
+        return container;
     }
 }
--- GamePod/Views/ContainerDetailsPage.xaml.cs
+++ GamePod/Views/ContainerDetailsPage.xaml.cs
@@ -1,18 +1,46 @@
-﻿using GamePod.ViewModels;
+﻿using System.Diagnostics;
+using Docker.DotNet.Models;
+using GamePod.Models;
+using GamePod.ViewModels;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.Windows.Security.AccessControl;
+using Newtonsoft.Json;
 
 namespace GamePod.Views;
 
 public sealed partial class ContainerDetailsPage : Page
 {
-    public ContainerDetailsViewModel ViewModel
-    {
-        get;
-    }
+    private Container container { get; set; }
+
+    public ContainerDetailsViewModel ViewModel { get; }
+    private string ProjectName { get; set; } = string.Empty;
+    private string ContainerImage { get; set; } = string.Empty;
+    private string ContainerID { get; set; } = string.Empty;
+    private string ContainerInspect { get; set; } = string.Empty;
 
     public ContainerDetailsPage()
     {
         ViewModel = App.GetService<ContainerDetailsViewModel>();
         InitializeComponent();
     }
+
+    #region Navigation
+    protected async override void OnNavigatedTo(NavigationEventArgs e)
+    {
+        base.OnNavigatedTo(e);
+
+        Debug.WriteLine($"ContainerDetailsPage.OnNavigatedTo: {e.Parameter}");
+
+        // Get the container from the parameter
+        container = e.Parameter as Container;
+
+        // Set the container name in the title
+        ProjectName = container.inspectResponse.Name;
+        ContainerImage = container.inspectResponse.Config.Image;
+        ContainerID = container.inspectResponse.Config.Hostname;
+        ContainerInspect = JsonConvert.SerializeObject(container.inspectResponse, Formatting.Indented);
+
+    }
+    #endregion
 }
